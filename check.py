75dc1b0569a29c1bc051010bedc87fa0dde56a22
shashankreddy <shashankreddy1309@gmail,com>
2025-06-30 22:16:24 +0530

    check

--- check.py
+++ check.py
@@ -1,1 +1,9 @@
-print("just checking the file")
+def check_unique(lst):
+    # Your code goes here
+    lst1=[]
+    for i in range(0,len(lst)):
+        for j in range(i+1,len(lst)):
+            if(lst[i]==lst[j]):
+                return False
+    else:
+        return True
